f1668272c59b0b89421fcb47354b8043c851345a
SehyeonKang <gedfgrdt@naver.com>
2024-01-28 00:32:35 +0900

    feat: 스트라이크, 볼 개수를 계산하는 추가

---
+++ java-baseball-6-main/src/main/java/baseball/domain/Referee.java
@@ -0,0 +1,33 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Referee {
+
+    int strikeCount;
+    int ballCount;
+
+    public void compareNumbers(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        for (int i = 0; i < computerNumbers.size(); i++) {
+            int computerNumber = computerNumbers.get(i);
+            int userNumber = userNumbers.get(i);
+            if (computerNumber == userNumber) {
+                strikeCount++;
+            }
+
+            if (computerNumbers.contains(userNumber)) {
+                ballCount++;
+            }
+        }
+
+        ballCount -= strikeCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+}
