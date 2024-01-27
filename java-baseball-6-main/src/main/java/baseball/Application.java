f349d629d5e7224c8f9d67b35910008a4a5cf6ac
SehyeonKang <gedfgrdt@naver.com>
2024-01-27 23:51:22 +0900

    feat: 볼, 스트라이크 관련 출력 기능 추가

--- java-baseball-6-main/src/main/java/baseball/Application.java
+++ java-baseball-6-main/src/main/java/baseball/Application.java
@@ -1,11 +1,12 @@
 package baseball;
 
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        String test = inputView.readNumbersView();
-        System.out.println(test);
+        OutputView outputView = new OutputView();
+        outputView.printStrikeAndBallCountView(1, 1);
     }
 }
--- java-baseball-6-main/src/main/java/baseball/view/OutputView.java
+++ java-baseball-6-main/src/main/java/baseball/view/OutputView.java
@@ -5,4 +5,20 @@ public class OutputView {
     public void printGameStartView() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void printStrikeAndBallCountView(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        if (ball > 0) {
+            System.out.print(ball + "볼 ");
+        }
+
+        if (strike > 0) {
+            System.out.print(strike + "스트라이크");
+        }
+        System.out.println();
+    }
 }
