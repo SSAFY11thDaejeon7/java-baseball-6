cd07c4a7ef716fbf2453a18a95248ac8f3d00146
jeonseong-gwon <jeonsg9904@gmail.com>
2024-02-02 14:48:54 +0900

    feat : 숫자를 입력받고 저장하는Baseball 생성

---
+++ java-baseball-6-main/src/main/java/baseball/domain/Baseball.java
@@ -0,0 +1,19 @@
+package baseball.domain;
+
+public class Baseball {
+
+    private int number;
+
+    public Baseball(Integer number) {
+        this.number = number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public void setNumber(int number){
+        this.number = number;
+    }
+
+}
