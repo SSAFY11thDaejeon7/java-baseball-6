2d0ba9ad10b8abe82219f390076f71ba24783aff
jeonseong-gwon <jeonsg9904@gmail.com>
2024-02-03 14:49:38 +0900

    Feat : 컨트롤러 로직 구현

---
+++ java-baseball-6-main/src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,50 @@
+package baseball.controller;
+
+import baseball.domain.Baseballs;
+import baseball.service.BaseballService;
+import baseball.util.RandomNumbersGenerator;
+import baseball.view.InputView;
+import baseball.view.Message;
+import baseball.view.OutputView;
+
+import java.util.List;
+
+public class BaseballController {
+    private Baseballs baseballGame;
+    private final BaseballService baseballService;
+    private boolean stopGame = false;
+
+    public BaseballController(){
+        baseballService =  new BaseballService();
+        baseballGame = new Baseballs(RandomNumbersGenerator.randomNumberGenerator());
+    }
+    public void run() {
+        System.out.println(Message.START_GAME_MESSAGE);
+        playGame();
+    }
+
+    public void playGame(){
+        while (!stopGame) {
+            List<Integer> userInput = InputView.gameInput();
+            String hintMessage = baseballService.createHintMessage(baseballGame, userInput);
+            OutputView.printHintMessage(hintMessage);
+            baseballGame = manageGameResult(baseballGame, userInput);
+        }
+    }
+
+    private Baseballs manageGameResult(Baseballs baseballGame, List<Integer> userInput) {
+        if (baseballService.isCorrect(baseballGame, userInput)) {
+            int gameType = OutputView.endGame();
+            //validateGameCode(gameType);
+
+            if (gameType == 1) {
+                baseballGame = new Baseballs(RandomNumbersGenerator.randomNumberGenerator());
+            }
+            if (gameType == 2) {
+                stopGame = true;
+            }
+        }
+
+        return baseballGame;
+    }
+}
