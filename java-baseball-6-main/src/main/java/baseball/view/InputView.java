383d411f6b41bc608c6ced1b36ba92deaefcab44
jeonseong-gwon <jeonsg9904@gmail.com>
2024-02-04 13:55:22 +0900

    Refactor : 구조 수정 및 문자열 상수 처리

--- java-baseball-6-main/src/main/java/baseball/view/InputView.java
+++ java-baseball-6-main/src/main/java/baseball/view/InputView.java
@@ -8,8 +8,17 @@ import java.util.List;
 public class InputView {
 
     public static List<Integer> gameInput() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.println(Message.NUMERIC_INPUT_MESSAGE);
         String str = Console.readLine();
         return Baseballs.changeStrToList(str);
+    }
+    public static int endGame() {
+        System.out.println(Message.WIN_MESSAGE);
+        System.out.println(Message.MENU_PROMPT);
+
+        String userInput = Console.readLine();
+        int restartType = Integer.parseInt(userInput);
+
+        return restartType;
     }
 }
