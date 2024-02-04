5ecbc9f386c5ee2640a73c443f61ae322f4e561f
mrcsbin <mrcsbin@naver.com>
2024-02-05 02:32:26 +0900

    feat: 입력값 검증 기능 구현

--- java-baseball-6-main/src/main/java/baseball/Application.java
+++ java-baseball-6-main/src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.model.GameModel;
+import baseball.controller.GameController;
+import baseball.view.GameView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameModel game = new GameModel();
+        GameView gameView = new GameView();
+        GameController gameController = new GameController(game, gameView);
+        gameController.startGame();
     }
 }
