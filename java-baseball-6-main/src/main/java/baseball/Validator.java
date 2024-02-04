dd966571119ef85c748109a84467b27e0a5c9ee1
Yebin <ferf99@naver.com>
2024-02-05 01:57:05 +0900

    feat: 사용자에게 숫자를 입력받고, 입력값에 대한 유효성 검사 기능 추가

---
+++ java-baseball-6-main/src/main/java/baseball/Validator.java
@@ -0,0 +1,58 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Validator {
+
+    private final static int INPUT_SIZE = 3;
+    private final static String NUMBERS = "123456789";
+    public final static String INPUT = "숫자를 입력해주세요 : ";
+    public final static String INPUT_SIZE_ERROR = "입력의 길이는 3자리여야 합니다.";
+    public final static String INPUT_TYPE_ERROR = "입력은 0을 제외한 숫자만 가능합니다.";
+    public final static String INPUT_DUPLICATION_ERROR = "입력은 서로 다른 숫자만 가능합니다.";
+
+
+
+    public static List<Integer> inputValidationCheck() {
+        System.out.print(INPUT);
+        String input = Console.readLine();
+        lengthCheck(input);
+        for (int i = 0; i < INPUT_SIZE; i++) {
+            String c = Character.toString(input.charAt(i));
+            typeCheck(c);
+            duplicationCheck(input, c);
+        }
+        return inputToArray(input);
+
+    }
+
+    public static void lengthCheck(String input) throws IllegalArgumentException {
+        if (input.length() != INPUT_SIZE)
+            throw new IllegalArgumentException(INPUT_SIZE_ERROR);
+    }
+
+    public static void typeCheck(String c) throws IllegalArgumentException {
+        if (!NUMBERS.contains(c))
+            throw new IllegalArgumentException(INPUT_TYPE_ERROR);
+    }
+
+    public static void duplicationCheck(String input, String c) {
+        if (INPUT_SIZE - input.replace(c, "").length() != 1) {
+            throw new IllegalArgumentException(INPUT_DUPLICATION_ERROR);
+        }
+    }
+
+    public static List<Integer> inputToArray(String input) {
+        List<Integer> result = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+
+}
