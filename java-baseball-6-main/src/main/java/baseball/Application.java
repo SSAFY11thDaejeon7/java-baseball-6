011c62bc772b16c028c310c2b8d17fc6f6422813
ensk26 <tjensk26@gmail.com>
2024-02-04 17:56:59 +0900

    feat: 실행 추가

--- java-baseball-6-main/src/main/java/baseball/Application.java
+++ java-baseball-6-main/src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
 public class Application {
+
+    static final Run run = Run.getInstance();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        run.play();
     }
 }
